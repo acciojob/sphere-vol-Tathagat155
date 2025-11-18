931063a3fdb23dd89fc48b5282ef2e6997a3fd32
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-18 15:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function volume_sphere() {
 	let r=Number(radius.value);
 	if(isNaN(r) || r<0){
 		
-		volume.value=NaN;
+		volume.value="NaN";
 	}
 		let result=((4/3)*(22/7)*(r*r*r));
 	    volume.value=result.toFixed(4);
