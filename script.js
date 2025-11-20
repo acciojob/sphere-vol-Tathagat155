80ec55fb81a87c315ecf2e99b05e732db230bd5d
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-20 11:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 let volume=document.getElementById("volume")
 function volume_sphere() {
     //Write your code here
-	let r=Number(radius.value);
-	if(isNaN(r) || r<0){
-		
+	let r=parseInt(radius.value);
+	if(isNaN(r) || r<0){	
 		volume.value="NaN";
+	}else{
+		let v = (4/3) * Math.PI * Math.pow(r, 3);
+	    volume.value=v.toFixed(4);
 	}
-		let result=((4/3)*(22/7)*(r*r*r));
-	    volume.value=result.toFixed(4);
-
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
